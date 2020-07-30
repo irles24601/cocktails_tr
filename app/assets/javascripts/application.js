08e39892a46245982fe281d5295ea9dc0bf99cd7
Toshiki Isawa <jack_the_fool@outlook.com>
2020-07-30 10:57:39 +0900

    [Update] My page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -77,3 +77,20 @@ jQuery(function($){
     isFitWidth: true  //親要素の幅に合わせてカラム数を自動調整
   });  
 });
+
+$(function(){
+  $('.tab-content>div').hide();
+  $('.tab-content>div').first().slideDown();
+    $('.tab-buttons span').click(function(){
+      var thisclass=$(this).attr('class');
+      $('#lamp').removeClass().addClass('#lamp').addClass(thisclass);
+      $('.tab-content>div').each(function(){
+        if($(this).hasClass(thisclass)){
+          $(this).fadeIn(800);
+        }
+        else{
+          $(this).hide();
+        }
+      });
+    });
+});
